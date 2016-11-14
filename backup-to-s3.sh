cf506a95aa94305b33a51c38756ad3f68e30c1f3
Ivan.Nginx <ivan.nginx@gmail.com>
2016-11-14 20:32:55 +0300

    Changed script structure.
    1. All variables now define in external source file (backup-config.sh).
    2. Add date to log, change DATESTAMP format for better sorting.
    3. Temporaily comment removing/moving old backups via S3 service.

--- backup-to-s3.sh
+++ backup-to-s3.sh
@@ -1,72 +1,86 @@
 #!/bin/sh
+# ================================================================== #
+# Shell script to backup databases and directories/files via s3cmd.
+# ================================================================== #
+# Version: 1.0.4
+# ================================================================== #
+# Parts copyright (c) 2012 woxxy https://github.com/woxxy/MySQL-backup-to-Amazon-S3
+# Parts copyright (c) 2015 betweenbrain https://github.com/betweenbrain/MySQL-backup-to-Amazon-S3
+# Updates at: https://github.com/ivan-nginx/Backup-to-Amazon-S3
+# This script is licensed under MIT license
+# ================================================================== #
+#source ./backup-config.sh
+#source /etc/backup-config.sh
+source ~/backup-config.sh
 
-# Updates etc at: https://github.com/woxxy/MySQL-backup-to-Amazon-S3
-# Under a MIT license
-
-# change these variables to what you need
-MYSQLROOT=root
-MYSQLPASS=password
-S3BUCKET=bucketname
-# the following line prefixes the backups with the defined directory. it must be blank or end with a /
-S3PATH=mysql_backup/
-
-# when running via cron, the PATHs MIGHT be different. If you have a custom/manual MYSQL install, you should set this manually like MYSQLDUMPPATH=/usr/local/mysql/bin/
-MYSQLDUMPPATH=
-#tmp path.
-TMP_PATH=/tmp
-
-DATESTAMP=$(date +".%m.%d.%Y")
+DATESTAMP=$(date +"%Y.%m.%d-")
 DAY=$(date +"%d")
 DAYOFWEEK=$(date +"%A")
 PERIOD=${1-day}
 
 if [ ${PERIOD} = "auto" ]; then
 	if [ ${DAY} = "01" ]; then
-        	PERIOD=month
+		PERIOD=month
 	elif [ ${DAYOFWEEK} = "Sunday" ]; then
-        	PERIOD=week
+		PERIOD=week
 	else
-       		PERIOD=day
-	fi	
+		PERIOD=day
+	fi
 fi
 
+echo "========================="
+echo "$(date +'%Y-%b-%d [%R]')"
+echo "========================="
 echo "Selected period: $PERIOD."
 
 # we want at least two backups, two months, two weeks, and two days
-echo "Removing old backup (2 ${PERIOD}s ago)..."
-s3cmd del --recursive s3://${S3BUCKET}/${S3PATH}previous_${PERIOD}/
-echo "Old backup removed."
+#echo "Removing old backup (2 ${PERIOD}s ago)..."
+#s3cmd del --recursive s3://${S3BUCKET}/${S3PATH}previous_${PERIOD}/
+#echo "Old backup removed."
 
-echo "Moving the backup from past $PERIOD to another folder..."
-s3cmd mv --recursive s3://${S3BUCKET}/${S3PATH}${PERIOD}/ s3://${S3BUCKET}/${S3PATH}previous_${PERIOD}/
-echo "Past backup moved."
+#echo "Moving the backup from past $PERIOD to another folder..."
+#s3cmd mv --recursive s3://${S3BUCKET}/${S3PATH}${PERIOD}/ s3://${S3BUCKET}/${S3PATH}previous_${PERIOD}/
+#echo "Past backup moved."
 
-# List all the databases
+# List all the databases, exclude standart databases
 DATABASES=`mysql -u root -p$MYSQLPASS -e "SHOW DATABASES;" | tr -d "| " | grep -v "\(Database\|information_schema\|performance_schema\|mysql\|test\)"`
 
 # Loop the databases
 for DB in $DATABASES; do
 
-    # dump database
-    echo "Starting backing up the database to a file..."
-    ${MYSQLDUMPPATH}mysqldump --quick --user=${MYSQLROOT} --password=${MYSQLPASS} ${DB} > ${TMP_PATH}${DB}.sql
-    echo "Done backing up the database to a file."
+	# dump database
+	echo "Starting backing up the database to a file..."
+	${MYSQLDUMPPATH}mysqldump --quick --user=${MYSQLROOT} --password=${MYSQLPASS} ${DB} > ${TMP_PATH}${DB}.sql
+	echo "Done backing up the database to a file."
 
-    echo "Starting compression..."
-    tar czf ${TMP_PATH}${DB}${DATESTAMP}.tar.gz ${TMP_PATH}${DB}.sql
-    echo "Done compressing the backup file."
+	echo "Starting compression..."
+	tar czf ${TMP_PATH}${DATESTAMP}${DB}.tar.gz ${TMP_PATH}${DB}.sql
+	echo "Done compressing the backup file."
 
-    # upload all databases
-    echo "Uploading the new backup..."
-    s3cmd put -f ${TMP_PATH}${DB}${DATESTAMP}.tar.gz s3://${S3BUCKET}/${S3PATH}${PERIOD}/
-    echo "New backup uploaded."
+	# upload all databases
+	echo "Uploading the new backup..."
+	s3cmd put -f ${TMP_PATH}${DATESTAMP}${DB}.tar.gz s3://${S3BUCKET}/${S3PATH}${PERIOD}/
+	echo "New backup uploaded."
 
-    echo "Removing the cache files..."
-    # remove databases dump
-    rm ${TMP_PATH}${DB}.sql
-    rm ${TMP_PATH}${DB}${DATESTAMP}.tar.gz
-    echo "Cache file removed..."
+	echo "Removing the cache files..."
+	# remove databases dump
+	rm ${TMP_PATH}${DB}.sql
+	rm ${TMP_PATH}${DATESTAMP}${DB}.tar.gz
+	echo "Cache file removed..."
 
 done;
+
+	# backup defined files/directories with exclude
+	echo "Starting compression directories..."
+	tar pzcf ${TMP_PATH}${DATESTAMP}${HOSTNAME}.tar.gz ${DIRS} ${EXCLUDE}
+	echo "Done compressing directories."
+
+	echo "Uploading the new backup..."
+	s3cmd put -f ${TMP_PATH}${DATESTAMP}${HOSTNAME}.tar.gz s3://${S3BUCKET}/${S3PATH}${PERIOD}/
+	echo "New backup uploaded."
+
+	echo "Removing the cache files..."
+	rm ${TMP_PATH}${DATESTAMP}${HOSTNAME}.tar.gz
+	echo "Cache file removed..."
 
 echo "All done."
